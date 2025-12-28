96ca5fa80220a69f5541ad6e7de39c1c52ae9dec
brentjlaf <brentjlaf@hotmail.com>
2025-12-28 07:54:49 -0700

    Require login for saving pages

--- CMS/modules/pages/save_page.php
+++ CMS/modules/pages/save_page.php
@@ -4,6 +4,8 @@ require_once __DIR__ . '/../../includes/auth.php';
 require_once __DIR__ . '/../../includes/data.php';
 require_once __DIR__ . '/../../includes/sanitize.php';
 require_once __DIR__ . '/../../includes/page_schedule.php';
+
+require_login();
 $pagesFile = __DIR__ . '/../../data/pages.json';
 $pages = read_json_file($pagesFile);
 if (!is_array($pages)) {
